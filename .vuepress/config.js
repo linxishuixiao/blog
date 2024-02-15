d166c85888e9f57c6cc06fc7775c90799ce52588
小于 <13271305+smailfish@user.noreply.gitee.com>
2024-02-15 09:57:52 +0800

    second push

---
+++ .vuepress/config.js
@@ -0,0 +1,186 @@
+module.exports = {
+  "title": "Ren's Blog",
+  "description": "...",
+  "dest": "public",
+  "plugins": [
+    [
+      //先安装在配置， npm install @vuepress-reco/vuepress-plugin-kan-ban-niang --save
+      "@vuepress-reco/vuepress-plugin-kan-ban-niang",
+      {
+        theme: ['haruto'],
+        clean: true,
+        width: 150,
+        height: 220
+      },
+      
+    ],
+    ["sakura", {
+      num: 20, // 默认数量
+      show: true, // 是否显示
+      zIndex: 99, // 层级
+      img: {
+        replace: false, // false 默认图 true 换图 需要填写httpUrl地址
+        httpUrl: '…' // 绝对路径
+      }
+    }],
+    ["cursor-effects",
+      {
+        size: 4, // size of the particle, default: 2
+        shape: "star", // ['star' | 'circle'], // shape of the particle, default: 'star'
+        zIndex: 999999999, // z-index property of the canvas, default: 999999999
+      }],
+    ["ribbon-animation", {
+      size: 90,   // 默认数据
+      opacity: 0.3,  //  透明度
+      zIndex: -99,   //  层级
+      opt: {
+        // 色带HSL饱和度
+        colorSaturation: "80%",
+        // 色带HSL亮度量
+        colorBrightness: "60%",
+        // 带状颜色不透明度
+        colorAlpha: 0.65,
+        // 在HSL颜色空间中循环显示颜色的速度有多快
+        colorCycleSpeed: 6,
+        // 从哪一侧开始Y轴 (top|min, middle|center, bottom|max, random)
+        verticalPosition: "center",
+        // 到达屏幕另一侧的速度有多快
+        horizontalSpeed: 200,
+        // 在任何给定时间，屏幕上会保留多少条带
+        ribbonCount: 2,
+        // 添加笔划以及色带填充颜色
+        strokeSize: 0,
+        // 通过页面滚动上的因子垂直移动色带
+        parallaxAmount: -0.5,
+        // 随着时间的推移，为每个功能区添加动画效果
+        animateSections: true
+      },
+      ribbonShow: false, //  点击彩带  true显示  false为不显示
+      ribbonAnimationShow: true  // 滑动彩带
+    }],
+    ['@vuepress-reco/vuepress-plugin-pagation', {
+      perPage: 5  // 每页展示条数
+    }],
+    // ["vuepress-plugin-nuggets-style-copy", {
+    //   copyText: "copy",
+    //   tip: {
+    //     content: "复制成功!"
+    //   }
+    // }]
+    // ['@vuepress-reco/vuepress-plugin-bulletin-popover', {
+    //   width: '300px', // 默认 260px
+    //   title: '一则消息',
+    //   body: [
+    //     {
+    //       type: 'title',
+    //       content: '这里是新人菜鸡前端的个人博客,分享日常生活、学习过程还有一些牢骚 🎉🎉🎉',
+    //     },
+    //     {
+    //       type: 'image',
+    //       src: '/wx.jpg'
+    //     }
+    //   ]
+    // }]
+  ],
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/head.jpeg"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "locales": {
+    "/": {
+      lang: "zh-CN",
+    },
+  },
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "主页",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "时间条",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "文档",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "vuepress-reco",
+            "link": "/docs/theme-reco/"
+          }
+        ]
+      },
+      {
+        "text": "与我联系",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/recoluan",
+            "icon": "reco-github"
+          }
+        ]
+      }
+    ],
+    "sidebar": {
+      "/docs/theme-reco/": [
+        "",
+        "theme",
+        "plugin",
+        "api"
+      ]
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 4,
+        "text": "分类"
+      },
+      "tag": {
+        "location": 3,
+        "text": "标签"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      },
+      {
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],
+    "logo": "/head.jpeg",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "Ren",
+    "authorAvatar": "/head.jpeg",
+    "record": "xxxx",
+    "startYear": "2023"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
